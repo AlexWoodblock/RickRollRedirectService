5b0b3c57060436f3413982a02aaf8df34c777b4a
Alexander Leontev <alexwoodblock@icloud.com>
2022-07-29 17:38:45 +0200

    Fix database connection

--- src/main/kotlin/com/woodblockwithoutco/rickroller/db/DatabaseFactory.kt
+++ src/main/kotlin/com/woodblockwithoutco/rickroller/db/DatabaseFactory.kt
@@ -6,25 +6,16 @@
 package com.woodblockwithoutco.rickroller.db
 
 import org.jetbrains.exposed.sql.Database
+import org.slf4j.LoggerFactory
 import java.net.URI
 
 /**
  * Create database from raw passed URL.
  */
 fun createDatabase(
-    rawUrl: String
+    url: String
 ): Database {
-    val uri = URI(rawUrl)
-
-    val userInfo = uri.userInfo.split(":")
-    val userName = userInfo[0]
-    val userPassword = userInfo[1]
-
-    val url = "jdbc:postgresql://" + uri.host + ':' + uri.port + uri.path
-
     return Database.connect(
-        url = url,
-        user = userName,
-        password = userPassword
+        url = url
     )
 }
